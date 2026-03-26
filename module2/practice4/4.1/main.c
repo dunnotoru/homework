cf22bef99a6ac9c4898272608cbcbcdfbcb6a453
dunnotoru <dunnotoru@yandex.ru>
2026-03-26 20:29:17 +0700

    implement linked list

---
+++ module2/practice4/4.1/main.c
@@ -0,0 +1,437 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "phonebook.h"
+
+#define A_FIELD_COUNT 5
+#define U_FIELD_COUNT 7
+
+ContactStorage* storage;
+
+typedef enum ReadStatus {
+  READ_OK = 0,
+  READ_CANCEL,
+  READ_INVALID,
+  READ_FAIL,
+} ReadStatus;
+
+typedef enum MenuStatus { MENU_OK = 0, MENU_FAIL } MenuStatus;
+
+void print_brief(Contact* contact);
+void print_details(Contact* contact);
+
+ReadStatus readline(char* prompt, char* value);
+ReadStatus readline_required(char* prompt, char* value);
+ReadStatus read_number(char* prompt, int* value);
+ReadStatus read_character(char* ch);
+
+MenuStatus add_menu();
+MenuStatus details_menu();
+MenuStatus edit_menu(Contact* c);
+MenuStatus delete_menu(Contact* c);
+MenuStatus edit_numbers(Contact* c);
+
+int main() {
+  storage = create_storage();
+  printf("Книга контактов!\n");
+  Contact* contact = create_contact("john", "doe", NULL);
+  Contact* contact2 = create_contact("oleg", "olegov", NULL);
+  add_contact(storage, contact);
+  add_contact(storage, contact2);
+
+  int running = 1;
+  while (running) {
+    size_t i = 0;
+    ListIterator it = it_begin(storage->data_head);
+    for (ListNode* node = it_current(&it); node != NULL; node = it_next(&it)) {
+      Contact* contact = get_node_value(node);
+      printf("[%zu] ", i + 1);
+      print_brief(contact);
+      i++;
+    }
+
+    printf("[A] Добавить, [M] Подробнее, [Q] Завершить\n> ");
+
+    char opt;
+    ReadStatus read_status = read_character(&opt);
+    switch (read_status) {
+      case READ_FAIL:
+        return -1;
+      default:
+        break;
+    }
+
+    MenuStatus menu_status = MENU_OK;
+    switch (opt) {
+      case 'A':
+        menu_status = add_menu();
+        break;
+      case 'M':
+        menu_status = details_menu();
+        break;
+      case 'Q':
+        running = 0;
+        break;
+      default:
+        printf("Неизвестная опция - %c.\n", opt);
+        break;
+    }
+
+    if (menu_status == MENU_FAIL) {
+      running = -1;
+    }
+  }
+
+  delete_storage(storage);
+  return 0;
+}
+
+void print_brief(Contact* contact) {
+  if (contact == NULL) {
+    printf("Контакт не найден.\n");
+    return;
+  }
+
+  printf("%s %s\n", contact->firstname, contact->lastname);
+}
+
+void print_details(Contact* contact) {
+  if (contact == NULL) {
+    printf("Контакт не найден.\n");
+    return;
+  }
+
+  printf("%s\n", contact->firstname);
+  printf("%s\n", contact->lastname);
+
+  if (contact->middlename != NULL) {
+    printf("%s\n", contact->middlename);
+  }
+
+  for (size_t i = 0; i < NUMBERS_SIZE; i++) {
+    printf("[%ld] %s\n", i + 1, contact->phone_numbers[i]);
+  }
+
+  if (contact->job != NULL) {
+    printf("%s\n", contact->job);
+  }
+
+  if (contact->position != NULL) {
+    printf("%s\n", contact->position);
+  }
+}
+
+ReadStatus readline(char* prompt, char* value) {
+  printf(prompt);
+  char buffer[FIELD_SIZE];
+  if (fgets(buffer, FIELD_SIZE, stdin) == NULL) {
+    return READ_FAIL;
+  }
+
+  if (buffer[0] == '\n') {
+    return READ_CANCEL;
+  }
+
+  sscanf(buffer, "%s", value);
+
+  return READ_OK;
+}
+
+ReadStatus readline_required(char* prompt, char* value) {
+  ReadStatus status = READ_CANCEL;
+  while (status != READ_OK) {
+    status = readline(prompt, value);
+    if (status == READ_FAIL) {
+      return status;
+    }
+  }
+
+  return READ_OK;
+}
+
+ReadStatus read_number(char* prompt, int* value) {
+  printf(prompt);
+  char buffer[FIELD_SIZE];
+  if (fgets(buffer, FIELD_SIZE, stdin) == NULL) {
+    return READ_FAIL;
+  }
+
+  if (buffer[0] == '\n') {
+    return READ_CANCEL;
+  }
+
+  if (buffer[0] == 'Q') {
+    return READ_CANCEL;
+  }
+
+  int number = -1;
+  sscanf(buffer, "%d", &number);
+
+  *value = number;
+  return READ_OK;
+}
+
+ReadStatus read_character(char* ch) {
+  char input = getchar();
+  *ch = input;
+  if (input == EOF) {
+    return READ_FAIL;
+  }
+
+  while ((input = getchar()) != '\n' && input != EOF);
+
+  if (input == EOF) {
+    return READ_FAIL;
+  }
+
+  return READ_OK;
+}
+
+MenuStatus edit_numbers(Contact* c) {
+  ReadStatus status = READ_INVALID;
+  int num = -1;
+  while (status != READ_OK) {
+    status = read_number("Введите номер слота [1-5]: ", &num);
+    switch (status) {
+      case READ_FAIL:
+        return MENU_FAIL;
+      default:
+        break;
+    }
+
+    if (0 > num || num <= 5) {
+      break;
+    }
+  }
+
+  char buffer[FIELD_SIZE];
+  status = readline("Введите номер: ", buffer);
+  if (status == READ_FAIL) {
+    return MENU_FAIL;
+  }
+
+  strncpy(c->phone_numbers[num - 1], buffer, FIELD_SIZE);
+  return MENU_OK;
+}
+
+MenuStatus add_menu() {
+  char name[FIELD_SIZE];
+  char lastname[FIELD_SIZE];
+
+  ReadStatus status = READ_OK;
+  status = readline_required("Имя*: ", name);
+  if (status == READ_FAIL) {
+    return MENU_FAIL;
+  }
+
+  status = readline_required("Фамилия*: ", lastname);
+  if (status == READ_FAIL) {
+    return MENU_FAIL;
+  }
+
+  char keys[A_FIELD_COUNT] = "nmjp";
+  char format[A_FIELD_COUNT + 1] = {0, 0, 0, 0};
+  char args[A_FIELD_COUNT][FIELD_SIZE] = {0};
+  char prompts[A_FIELD_COUNT][FIELD_SIZE] = {
+      "Телефон: ", "Отчество: ", "Место работы: ", "Должность: "};
+
+  size_t args_size = 0;
+  for (size_t i = 0; i < A_FIELD_COUNT - 1; i++) {
+    char buffer[FIELD_SIZE];
+    status = readline(prompts[i], buffer);
+    switch (status) {
+      case READ_FAIL:
+        return MENU_FAIL;
+      case READ_OK:
+        format[args_size] = keys[i];
+        strcpy(args[args_size++], buffer);
+      default:
+        break;
+    }
+  }
+
+  Contact* c;
+  switch (args_size) {
+    case 0:
+      c = create_contact(name, lastname, NULL);
+      break;
+    case 1:
+      c = create_contact(name, lastname, format, args[0]);
+      break;
+    case 2:
+      c = create_contact(name, lastname, format, args[0], args[1]);
+      break;
+    case 3:
+      c = create_contact(name, lastname, format, args[0], args[1], args[2]);
+      break;
+    case 4:
+      c = create_contact(name, lastname, format, args[0], args[1], args[2],
+                         args[3]);
+      break;
+  }
+
+  add_contact(storage, c);
+
+  printf("\n");
+  return MENU_OK;
+}
+
+MenuStatus details_menu() {
+  int id = -1;
+  while (1) {
+    ReadStatus status = READ_INVALID;
+    while (status != READ_OK) {
+      status =
+          read_number("Введите номер контакта (Q чтобы отказаться): ", &id);
+      switch (status) {
+        case READ_FAIL:
+          return MENU_FAIL;
+        case READ_INVALID:
+          printf("Неправильный ввод.\n");
+          break;
+        case READ_CANCEL:
+          return MENU_OK;
+        default:
+          break;
+      }
+    }
+
+    if (status == READ_OK && id > 0 && (size_t)id < storage->size + 1) {
+      break;
+    } else {
+      printf("Неверный ввод.\n");
+    }
+  }
+
+  id -= 1;
+
+  ListIterator it = it_begin(storage->data_head);
+  int i = 0;
+  ListNode* node;
+  for (node = it_current(&it); node != NULL && i < id; node = it_next(&it)) {
+    i++;
+  }
+
+  Contact* c = get_node_value(node);
+  if (i != id || c == NULL) {
+    printf("Контакт не найден.\n");
+    return MENU_OK;
+  }
+
+  while (1) {
+    print_details(c);
+    printf(
+        "[E] Изменить, "
+        "[N] Изменить номер, "
+        "[D] Удалить контакт, "
+        "[Q] Назад\n> ");
+
+    char opt = 0;
+    if (read_character(&opt) == READ_FAIL) {
+      return MENU_FAIL;
+    }
+
+    MenuStatus status = MENU_OK;
+
+    switch (opt) {
+      case 'E':
+        status = edit_menu(c);
+        break;
+      case 'D':
+        status = delete_menu(c);
+        return status;
+      case 'N':
+        status = edit_numbers(c);
+        break;
+      case 'Q':
+        return MENU_OK;
+      default:
+        printf("Неизвестная опция - %u.\n", opt);
+        break;
+    }
+
+    if (status == MENU_FAIL) {
+      return MENU_FAIL;
+    }
+  }
+}
+
+MenuStatus edit_menu(Contact* c) {
+  ReadStatus status = READ_OK;
+
+  char keys[U_FIELD_COUNT] = "flnmjp";
+  char format[U_FIELD_COUNT] = {0, 0, 0, 0, 0, 0};
+  char args[U_FIELD_COUNT][FIELD_SIZE] = {0};
+  char prompts[U_FIELD_COUNT][FIELD_SIZE] = {
+      "Имя: ",      "Фамилия: ",      "Телефон: ",
+      "Отчество: ", "Место работы: ", "Должность: "};
+
+  size_t args_size = 0;
+
+  for (size_t i = 0; i < U_FIELD_COUNT - 1; i++) {
+    char buffer[FIELD_SIZE];
+    status = readline(prompts[i], buffer);
+    switch (status) {
+      case READ_FAIL:
+        return MENU_FAIL;
+      case READ_OK:
+        format[args_size] = keys[i];
+        strncpy(args[args_size++], buffer, FIELD_SIZE);
+      default:
+        break;
+    }
+  }
+
+  switch (args_size) {
+    case 1:
+      update_contact(c, format, args[0]);
+      break;
+    case 2:
+      update_contact(c, format, args[0], args[1]);
+      break;
+    case 3:
+      update_contact(c, format, args[0], args[1], args[2]);
+      break;
+    case 4:
+      update_contact(c, format, args[0], args[1], args[2], args[3]);
+      break;
+    case 5:
+      update_contact(c, format, args[0], args[1], args[2], args[3], args[4]);
+      break;
+    case 6:
+      update_contact(c, format, args[0], args[1], args[2], args[3], args[4],
+                     args[5]);
+      break;
+  }
+
+  printf("\n");
+  return MENU_OK;
+}
+
+MenuStatus delete_menu(Contact* c) {
+  while (1) {
+    printf("Вы уверены? [y/n]: ");
+    char opt;
+    ReadStatus status = read_character(&opt);
+
+    switch (status) {
+      case READ_CANCEL:
+        return MENU_OK;
+      case READ_FAIL:
+        return MENU_FAIL;
+      case READ_INVALID:
+        continue;
+      default:
+        break;
+    }
+
+    switch (opt) {
+      case 'y':
+        remove_contact(storage, c);
+        return MENU_OK;
+      case 'n':
+        return MENU_OK;
+    }
+  }
+}
---
+++ module2/practice4/4.1/phonebook.c
@@ -0,0 +1,168 @@
+#include "phonebook.h"
+
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void update_value(Contact* c, char key, const char* value);
+
+ContactStorage* create_storage() {
+  ContactStorage* storage = malloc(sizeof(ContactStorage));
+  storage->data_head = NULL;
+  storage->size = 0;
+  storage->nextid = 1;
+  return storage;
+}
+
+void delete_storage(ContactStorage* storage) {
+  if (storage->data_head == NULL) {
+    free(storage);
+    return;
+  }
+
+  ListNode* cur = storage->data_head;
+  do {
+    ListNode* tmp = cur;
+    Contact* c = get_node_value(tmp);
+    delete_contact(c);
+
+    cur = cur->next;
+    cur->prev = NULL;
+    free(tmp);
+  } while (cur != storage->data_head && cur != NULL);
+
+  free(storage);
+}
+
+int add_cmp(void* left, void* right) {
+  return strcmp(((Contact*)left)->lastname, ((Contact*)right)->lastname);
+}
+
+void add_contact(ContactStorage* storage, Contact* contact) {
+  if (storage == NULL) {
+    return;
+  }
+
+  storage->data_head = insert_node(storage->data_head, contact, add_cmp);
+
+  contact->id = storage->nextid++;
+  storage->size++;
+}
+
+Contact* remove_contact(ContactStorage* storage, Contact* contact) {
+  storage->data_head = remove_node(storage->data_head, contact);
+  return contact;
+}
+
+Contact* get_contact(const ContactStorage* storage, int id) {
+  ListIterator it = it_begin(storage->data_head);
+  for (ListNode* node = it_current(&it); node != NULL; node = it_next(&it)) {
+    Contact* c = get_node_value(node);
+    printf("%s\n", c->firstname);
+    if (id == c->id) {
+      return c;
+    }
+  }
+
+  return NULL;
+}
+
+Contact* create_contact(const char* firstname, const char* lastname,
+                        const char* data_format, ...) {
+  if (firstname == NULL || lastname == NULL) {
+    return NULL;
+  }
+
+  Contact* c = calloc(1, sizeof(Contact));
+  if (c == NULL) {
+    return NULL;
+  }
+
+  size_t size = strlen(firstname);
+  c->firstname = calloc(size + 1, sizeof(char));
+  strcpy(c->firstname, firstname);
+
+  size = strlen(lastname);
+  c->lastname = calloc(size + 1, sizeof(char));
+  strcpy(c->lastname, lastname);
+
+  c->middlename = NULL;
+  c->job = NULL;
+  c->position = NULL;
+  c->id = -1;
+
+  if (data_format == NULL) {
+    return c;
+  }
+
+  va_list args;
+  va_start(args, data_format);
+  size_t format_size = strlen(data_format);
+
+  for (size_t i = 0; i < format_size; i++) {
+    char* value = va_arg(args, char*);
+    printf("%ld %s\n", i, value);
+    update_value(c, data_format[i], value);
+  }
+
+  va_end(args);
+  return c;
+}
+
+int update_contact(Contact* c, const char* data_format, ...) {
+  if (data_format == NULL || c == NULL) {
+    return 0;
+  }
+
+  va_list args;
+  va_start(args, data_format);
+  size_t format_size = strlen(data_format);
+
+  for (size_t i = 0; i < format_size; i++) {
+    char* value = va_arg(args, char*);
+    update_value(c, data_format[i], value);
+  }
+
+  va_end(args);
+
+  return 0;
+}
+
+void delete_contact(Contact* contact) {
+  free(contact->firstname);
+  free(contact->lastname);
+  free(contact->middlename);
+  free(contact->job);
+  free(contact->position);
+}
+
+void update_value(Contact* c, char key, const char* value) {
+  size_t size = (strlen(value) + 1) * sizeof(char);
+  switch (key) {
+    case 'f':
+      c->firstname = realloc(c->firstname, size);
+      strcpy(c->firstname, value);
+      break;
+    case 'l':
+      c->lastname = realloc(c->lastname, size);
+      strcpy(c->lastname, value);
+      break;
+    case 'm':
+      c->middlename = realloc(c->middlename, size);
+      strcpy(c->middlename, value);
+      break;
+    case 'n':
+      strcpy(c->phone_numbers[0], value);
+      break;
+    case 'j':
+      c->job = realloc(c->job, size);
+      strcpy(c->job, value);
+      break;
+    case 'p':
+      c->position = realloc(c->position, size);
+      strcpy(c->position, value);
+      break;
+  }
+}
